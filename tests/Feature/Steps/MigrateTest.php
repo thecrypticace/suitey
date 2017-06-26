d2d72c7e926d09f9615d339867d19f00e311f200
Jordan Pittman <jordan@cryptica.me>
2017-06-25 21:35:02 -0400

    Refactor and enable tests

--- tests/Feature/Steps/MigrateTest.php
+++ tests/Feature/Steps/MigrateTest.php
@@ -10,51 +10,56 @@ class MigrateTest extends TestCase
     /** @test */
     public function it_can_migrate_the_default_database()
     {
-        $this->markTestIncomplete("TODO: Artisan CLI not set up for testing yet");
-
         $this->suitey->add(new Steps\Migrate);
+        $this->suitey->add(new Steps\RunCode("Asserting", function () {
+            $this->assertDatabaseHas("default_tests", ["id" => 1]);
+        }));
 
         $result = $this->artisan("test");
         $result->assertStatus(0);
         $result->assertStepOutput([
-            "[1/2] Migrate database",
-            "[2/2] Run PHPUnit",
+            "[1/3] Migrate database",
+            "[2/3] Asserting",
+            "[3/3] Run PHPUnit",
         ]);
-
-        // Check for database tables
     }
 
     /** @test */
     public function it_can_migrate_a_custom_database()
     {
-        $this->markTestIncomplete("TODO: Artisan CLI not set up for testing yet");
-
         $this->suitey->add(new Steps\Migrate("foo"));
+        $this->suitey->add(new Steps\RunCode("Asserting", function () {
+            $this->assertDatabaseHas("default_tests", ["id" => 1], "foo");
+        }));
 
         $result = $this->artisan("test");
         $result->assertStatus(0);
         $result->assertStepOutput([
-            "[1/2] Migrate foo",
-            "[2/2] Run PHPUnit",
+            "[1/3] Migrate foo",
+            "[2/3] Asserting",
+            "[3/3] Run PHPUnit",
         ]);
-
-        // Check for database tables
     }
 
     /** @test */
     public function it_can_migrate_a_custom_database_with_a_custom_path()
     {
-        $this->markTestIncomplete("TODO: Artisan CLI not set up for testing yet");
-
-        $this->suitey->add(new Steps\Migrate("bar", "database/migrations/bar"));
+        $this->suitey->add(new Steps\Migrate("bar", "{$this->relativeFixturePath()}/database/migrations/bar"));
+        $this->suitey->add(new Steps\RunCode("Asserting", function () {
+            $this->assertDatabaseHas("bar_tests", ["id" => 1], "bar");
+        }));
 
         $result = $this->artisan("test");
         $result->assertStatus(0);
         $result->assertStepOutput([
-            "[1/2] Migrate bar",
-            "[2/2] Run PHPUnit",
+            "[1/3] Migrate bar",
+            "[2/3] Asserting",
+            "[3/3] Run PHPUnit",
         ]);
+    }
 
-        // Check for database tables
+    private function relativeFixturePath()
+    {
+        return "../../../../tests/Fixture";
     }
 }
