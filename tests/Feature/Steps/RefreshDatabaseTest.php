7eeaf95161eba3560d3dc2107548a4de37200cec
Jordan Pittman <jordan@cryptica.me>
2017-07-06 21:02:40 -0400

    Refactor refresh database test

--- tests/Feature/Steps/RefreshDatabaseTest.php
+++ tests/Feature/Steps/RefreshDatabaseTest.php
@@ -6,74 +6,99 @@ use TheCrypticAce\Suitey\Steps;
 
 class RefreshDatabaseTest extends TestCase
 {
-    /** @test */
-    public function it_can_refresh_the_default_database()
-    {
-        $this->suitey->add(new Steps\RefreshDatabase);
-        $this->suitey->add(new Steps\RunCode("Asserting", function () {
-            $this->assertDatabaseHas("default_tests", ["id" => 1]);
-        }));
+    use RunStepTests;
 
-        $result = $this->artisan("test");
-        $result->assertStatus(0);
-        $result->assertOutputContains([
-            "[1/3] Refreshing database",
-            "[2/3] Asserting",
-            "[3/3] Run PHPUnit",
-        ]);
+    protected function name()
+    {
+        return "refresh database";
     }
 
-    /** @test */
-    public function it_can_refresh_a_custom_database()
+    protected function steps()
     {
-        $this->suitey->add(new Steps\RefreshDatabase("foo"));
-        $this->suitey->add(new Steps\RunCode("Asserting", function () {
-            $this->assertDatabaseHas("default_tests", ["id" => 1], "foo");
-        }));
+        yield "default database" => [
+            "steps" => [
+                [
+                    "class" => Steps\RefreshDatabase::class,
+                    "options" => [],
+                ],
+            ],
 
-        $result = $this->artisan("test");
-        $result->assertStatus(0);
-        $result->assertOutputContains([
-            "[1/3] Refreshing foo",
-            "[2/3] Asserting",
-            "[3/3] Run PHPUnit",
-        ]);
-    }
+            "status" => 0,
+            "output" => [
+                "[1/3] Refreshing database",
+                "[2/3] Asserting",
+                "[3/3] Run PHPUnit",
+            ],
 
-    /** @test */
-    public function it_can_refresh_a_custom_database_with_a_custom_path()
-    {
-        $this->suitey->add(new Steps\RefreshDatabase("bar", "{$this->relativeFixturePath()}/database/migrations/bar"));
-        $this->suitey->add(new Steps\RunCode("Asserting", function () {
-            $this->assertDatabaseHas("bar_tests", ["id" => 1], "bar");
-        }));
+            "database" => [
+                ["connection" => null, "table" => "default_tests", "attributes" => ["id" => 1]],
+            ],
+        ];
 
-        $result = $this->artisan("test");
-        $result->assertStatus(0);
-        $result->assertOutputContains([
-            "[1/3] Refreshing bar",
-            "[2/3] Asserting",
-            "[3/3] Run PHPUnit",
-        ]);
-    }
+        yield "custom database" => [
+            "steps" => [
+                [
+                    "class" => Steps\RefreshDatabase::class,
+                    "options" => ["database" => "foo"],
+                ],
+            ],
 
-    /** @test */
-    public function it_can_refresh_multiple_sets_of_migrations_with_duplicate_class_names()
-    {
-        $this->suitey->add(new Steps\RefreshDatabase("foo", "{$this->relativeFixturePath()}/database/migrations/dup1"));
-        $this->suitey->add(new Steps\RefreshDatabase("bar", "{$this->relativeFixturePath()}/database/migrations/dup2"));
-        $this->suitey->add(new Steps\RunCode("Asserting", function () {
-            $this->assertDatabaseHas("dup1_tests", ["id" => 1], "foo");
-            $this->assertDatabaseHas("dup2_tests", ["id" => 1], "bar");
-        }));
+            "status" => 0,
+            "output" => [
+                "[1/3] Refreshing foo",
+                "[2/3] Asserting",
+                "[3/3] Run PHPUnit",
+            ],
 
-        $result = $this->artisan("test");
-        $result->assertStatus(0);
-        $result->assertOutputContains([
-            "[1/4] Refreshing foo",
-            "[2/4] Refreshing bar",
-            "[3/4] Asserting",
-            "[4/4] Run PHPUnit",
-        ]);
+            "database" => [
+                ["connection" => "foo", "table" => "default_tests", "attributes" => ["id" => 1]],
+            ],
+        ];
+
+        yield "custom database + custom path" => [
+            "steps" => [
+                [
+                    "class" => Steps\RefreshDatabase::class,
+                    "options" => ["database" => "bar", "path" => "{$this->relativeFixturePath()}/database/migrations/bar"],
+                ],
+            ],
+
+            "status" => 0,
+            "output" => [
+                "[1/3] Refreshing bar",
+                "[2/3] Asserting",
+                "[3/3] Run PHPUnit",
+            ],
+
+            "database" => [
+                ["connection" => "bar", "table" => "bar_tests", "attributes" => ["id" => 1]],
+            ],
+        ];
+
+        yield "multiple migrations with duplicate class names" => [
+            "steps" => [
+                [
+                    "class" => Steps\RefreshDatabase::class,
+                    "options" => ["database" => "foo", "path" => "{$this->relativeFixturePath()}/database/migrations/dup1"],
+                ],
+                [
+                    "class" => Steps\RefreshDatabase::class,
+                    "options" => ["database" => "bar", "path" => "{$this->relativeFixturePath()}/database/migrations/dup2"],
+                ],
+            ],
+
+            "status" => 0,
+            "output" => [
+                "[1/4] Refreshing foo",
+                "[2/4] Refreshing bar",
+                "[3/4] Asserting",
+                "[4/4] Run PHPUnit",
+            ],
+
+            "database" => [
+                ["connection" => "foo", "table" => "dup1_tests", "attributes" => ["id" => 1]],
+                ["connection" => "bar", "table" => "dup2_tests", "attributes" => ["id" => 1]],
+            ],
+        ];
     }
 }
